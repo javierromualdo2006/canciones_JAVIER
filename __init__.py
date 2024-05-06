9678f3f4d11a71e2835820d3c4cb0dd4e5ff7fd0
javierromualdo2006 <javieragustinromualdo2006@gmail.com>
2024-05-06 12:05:52 -0300

    haciendo el ejercicio 4

--- __init__.py
+++ __init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
 
-app = Flask(__name__)
+def create_app():
 
+    app = Flask(__name__)
+
+    from flask import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return app
+
+
+
+
